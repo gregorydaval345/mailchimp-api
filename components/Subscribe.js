e35c9f3dbf801afc1aa501f218b5df10d160bae3
gregorydaval345 <davalgregory69@gmail.com>
2022-06-15 13:04:48 +0100

    export default stuff

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const Subscribe = () => {
+function Subscribe() {
 
   const [email, setEmail] = useState("");
   const [state, setState] = useState("IDLE");
@@ -55,4 +55,6 @@ const Subscribe = () => {
       )}
     </div>
   );
-};
+}
+
+export default Subscribe;
