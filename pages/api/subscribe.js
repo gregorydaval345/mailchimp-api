19ee767497ba3116d7421db0d7c064dc70bd7d99
gregorydaval345 <davalgregory69@gmail.com>
2022-06-15 13:16:46 +0100

    Edited error message

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -50,7 +50,7 @@ export default async (req, res) => {
     return res.status(201).json({ error: null });
   } catch (error) {
     return res.status(400).json({
-      error: `Oops, something went wrong... Send me an email at uriklar@gmail.com and I'll add you to the list.`,
+      error: `Oops, something went wrong... Send an email to dontworryaboutit@gmail.com and we'll you get you sorted in no time.`,
     });
 
   }
